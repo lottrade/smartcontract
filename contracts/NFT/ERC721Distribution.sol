3e08a69f2bfe41d413812f4b05d641ea26cb7f93
taraskush <ktp30002@gmail.com>
2021-11-26 13:19:09 +0200

    UPDATE | nft contract

--- contracts/NFT/ERC721Distribution.sol
+++ contracts/NFT/ERC721Distribution.sol
@@ -6,139 +6,82 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-//v0.0.2
+//v0.0.3
 contract ERC721Distribution is ERC721URIStorage, Ownable {
-    uint256 public tokenCounter;
-
-    string startNftUrl = 'https://ipfs.io/ipfs/QmaSYZg9up7mzNuCk4iUCSPSqWqn3BWuXbBKfHiytN4Mue';
-    string proNftUrl = 'https://ipfs.io/ipfs/QmYw2pxEbKHVu8yQhu3e6HFCdgudzCVmnk3BL8DXfKN8qV';
-    string expertNftUrl = 'https://ipfs.io/ipfs/QmYtRxDCkZBwAmKmG8YiVpXWXfgfe9e9JJV4ioLeYwBmzc';
-    string partnerNftUrl = 'https://ipfs.io/ipfs/QmVBSLXVmMPfxVw4KDzxZP1dr3NxB7hmNEVTUf2zbWEbD3';
-
-    uint64 startNftCouner = 0;
-    uint64 proNftCouner = 0;
-    uint64 expertNftCouner = 0;
-    uint64 partnerNftCouner = 0;
-
-    uint64 startNftEmission = 4500;
-    uint64 proNftEmission = 1500;
-    uint64 expertNftEmission = 500;
-    uint64 partnerNftEmission = 100;
+    string startNftIpfsHash = "QmaSYZg9up7mzNuCk4iUCSPSqWqn3BWuXbBKfHiytN4Mue";
+    string proNftIpfsHash = "QmYw2pxEbKHVu8yQhu3e6HFCdgudzCVmnk3BL8DXfKN8qV";
+    string expertNftIpfsHash = "QmYtRxDCkZBwAmKmG8YiVpXWXfgfe9e9JJV4ioLeYwBmzc";
+    string partnerNftIpfsHash = "QmVBSLXVmMPfxVw4KDzxZP1dr3NxB7hmNEVTUf2zbWEbD3";
 
     mapping(address => bool) public trustedMinter;
 
-    mapping(address => uint256[]) public ovnerStartNft;
-    mapping(address => uint256[]) public ovnerProNft;
-    mapping(address => uint256[]) public ovnerExpertNft;
-    mapping(address => uint256[]) public ovnerPartnerNft;
+    mapping(address => uint256[]) public ovnerNft;
 
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_)  {
         trustedMinter[msg.sender] = true;
-        tokenCounter = 0;
     }
 
-     modifier checktrustedMinter() {
+     modifier checkTrustedMinter() {
         require(trustedMinter[msg.sender], "Trusted address only");
         _;
     }
 
-    modifier checkNftType(string memory nftType) {
-        require(
-            keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("start"))) ||
-            keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("pro"))) || 
-            keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("expert"))) || 
-            keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("partner"))),
-            "ERC721Distribution:: Unavailable nft type for mint"
-        );
-        _;
-    }
-
-    modifier chectNftEmission(string memory nftType) {
+    function updateNftUrl(string memory nftType, string memory newNftUrl) external onlyOwner {
         if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("start")))) {
-            require(startNftCouner < startNftEmission, "ERC721Distribution:: Over-emission for start nft");
+            startNftIpfsHash = newNftUrl;
         }
         else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("pro")))) {
-            require(proNftCouner < proNftEmission, "ERC721Distribution:: Over-emission for pro nft");
+            proNftIpfsHash = newNftUrl;
         }
         else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("expert")))) {
-            require(expertNftCouner < expertNftEmission, "ERC721Distribution:: Over-emission for expert nft");
+            expertNftIpfsHash = newNftUrl;
         }
         else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("partner")))) {
-            require(partnerNftCouner < partnerNftEmission, "ERC721Distribution:: Over-emission for partner nft");
-        }
-        _;
-    }
-
-    function updateNftEmission(string memory nftType, uint64 newEmission) external checkNftType(nftType) onlyOwner {
-        if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("start")))) {
-            startNftEmission = newEmission;
-        }
-        else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("pro")))) {
-            proNftEmission = newEmission;
-        }
-        else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("expert")))) {
-            expertNftEmission = newEmission;
-        }
-        else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("partner")))) {
-            partnerNftEmission = newEmission;
+            partnerNftIpfsHash = newNftUrl;
         }
     }
 
-    function updateNftUrl(string memory nftType, string memory newNftUrl) external checkNftType(nftType) onlyOwner {
-        if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("start")))) {
-            startNftUrl = newNftUrl;
-        }
-        else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("pro")))) {
-            proNftUrl = newNftUrl;
-        }
-        else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("expert")))) {
-            expertNftUrl = newNftUrl;
-        }
-        else if (keccak256(abi.encodePacked((nftType))) == keccak256(abi.encodePacked(("partner")))) {
-            partnerNftUrl = newNftUrl;
-        }
-    }
+    function mint(address _to, uint256 _tokenId) external checkTrustedMinter() returns (uint256) {
+        string memory nftIpfsHash = _getUrlForMintNft(_tokenId);
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, nftIpfsHash);
 
-    function mint(address to, string memory nftType) external checktrustedMinter() checkNftType(nftType) chectNftEmission(nftType) returns (uint256) {
-        uint256 newItemId = tokenCounter;
-        string memory nftUrl = _getUrlForMintNft(nftType);
-
-        _safeMint(to, newItemId);
-        _setTokenURI(newItemId, nftUrl);
-        tokenCounter = tokenCounter + 1;
-
-        _incrementNftCounterAndAddOwner(nftType, newItemId);
-
-        return newItemId;
+        ovnerNft[_to].push(_tokenId);
+        return _tokenId;
     }
 
     function setMinterStatus(address _minter, bool _isTrusted) external onlyOwner {
         trustedMinter[_minter] = _isTrusted;
     }
 
-    function _getUrlForMintNft(string memory _nftType) internal view returns(string memory) {
-        string memory nftUrl;
-        if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("start")))) nftUrl = startNftUrl;
-        else if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("pro")))) nftUrl = proNftUrl;
-        else if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("expert")))) nftUrl = expertNftUrl;
-        else if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("partner")))) nftUrl = partnerNftUrl;
+    function getOvnerNfts(address _ovner) external view returns(address, uint256[] memory) {
+        uint256[] memory ovnerNfts = ovnerNft[_ovner];
+        uint256[] memory nfts = new uint[](ovnerNfts.length);
 
-        return nftUrl;
+        for (uint i = 0; i < ovnerNfts.length; i++) {
+            nfts[i] = ovnerNfts[i];
+        }
+
+        return (_ovner, nfts);
     }
 
-    function _incrementNftCounterAndAddOwner(string memory _nftType, uint256 itemId) internal {
-        if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("start")))) {
-            startNftCouner += 1;
-            ovnerStartNft[msg.sender].push(itemId);
-        } else if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("pro")))) {
-            proNftCouner += 1;
-            ovnerProNft[msg.sender].push(itemId);
-        } else if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("expert")))) {
-            expertNftCouner += 1;
-            ovnerExpertNft[msg.sender].push(itemId);
-        } else if (keccak256(abi.encodePacked((_nftType))) == keccak256(abi.encodePacked(("partner")))) {
-            partnerNftCouner += 1;
-            ovnerPartnerNft[msg.sender].push(itemId);
-        }
+    function _getUrlForMintNft(uint256 _tokenId) internal view returns(string memory) {
+        require(_tokenId >= 0 && _tokenId <= 6599, "ERC721Distribution: unavailable itemId");
+        string memory nftIpfsHash;
+
+        if (_tokenId >= 0 && _tokenId <= 4499) nftIpfsHash = startNftIpfsHash;
+        else if (_tokenId > 4499 && _tokenId <= 5999) nftIpfsHash = proNftIpfsHash;
+        else if (_tokenId > 5999 && _tokenId <= 6499) nftIpfsHash = expertNftIpfsHash;
+        else if (_tokenId > 6499 && _tokenId <= 6599) nftIpfsHash = partnerNftIpfsHash;
+
+        return nftIpfsHash;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+
+    function baseURI() external view  returns (string memory) {
+        return _baseURI();
     }
 }
